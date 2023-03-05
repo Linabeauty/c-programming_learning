72d0ba3298ef296c35f0b4b9e02146fddb848aba
linabeauty <cabamiyo@gmail.com>
2023-03-05 16:56:13 +0000

    hello

---
+++ 0x00-helloworld_revision/0-hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//create a program that print the word in quotation//
+	int main;
+	printf("hello world");
+	printf("write out hello, world");
+	return (0);
+}

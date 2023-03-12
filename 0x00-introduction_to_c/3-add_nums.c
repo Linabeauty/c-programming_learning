05bc3535c073b7821f896fa118a7811bd738c5bf
linabeauty <cabamiyo@gmail.com>
2023-03-12 07:39:28 +0000

    Addition of numbers

---
+++ 0x00-introduction_to_c/3-add_nums.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* enter two numbers and find their sum */
+
+int main()
+{
+	int x = 200, y = 450;
+	scanf("%d%d", &x, &y);
+	if (x + y)
+	{
+		printf("sum = %d\n", 200 + 450);
+	}
+	return (0);
+}

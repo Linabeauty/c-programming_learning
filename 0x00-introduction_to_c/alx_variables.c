073badad4fb29b61de068eaea33f0ef2b2ded313
linabeauty <cabamiyo@gmail.com>
2023-03-14 21:04:51 +0000

    variables

---
+++ 0x00-introduction_to_c/alx_variables.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/* main - it assign a rand number to the variable
+ *
+ * return: Positive if the number is greater than 0, if is 0 is zero and if the number is less than 0 is negative.
+ */
+int main()
+{
+	int x, y, z; 
+
+	    printf("Enter a random number :: \n");
+	    scanf("%d", &x);
+	if (x > 0)
+	{
+		printf("x is positive \n");
+	}
+	if (y < 0)
+	{
+		printf("y is negative \n");
+	}
+	else
+	{
+		printf("z = zero \n");
+	}
+		return (0);
+}	

75a673cbe8b349014c0552c8eabe16edfc486173
linabeauty <cabamiyo@gmail.com>
2023-03-14 15:52:12 +0000

    centimeter to meter

--- 0x00-introduction_to_c/length_in_centimeter.c
+++ 0x00-introduction_to_c/length_in_centimeter.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
-/* ipput length in centimeter and convert it to meter and kilometer */
-
-int main()
+int main(void)
 {
-	int length , centimeter, meter, kilometer;
-	printf("Enter the a value in centimeter amd convert it to meter and kilometer");
-	scanf(:x
+	float c, m, k;
+	/* c = centimeter, m = meter, k = kilometer */
+
+	printf("Enter the length in centimeter::\n");
+	scanf("%f", &c);
+
+	/* It will convert centimeters into metres and kilometers */
+	m = (float)(c / 100);
+	k = (float)(c / 100000);
+
+	/* It will print the final output */
+	printf("\nLength in Meter = %f meter \n", m);
+	printf("Length in Kilometer = %f kilometer\n", k);
+	return (0);
+}

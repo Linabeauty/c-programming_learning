63e6336fa4baaf053524072dc2027e1b12a4ca31
linabeauty <cabamiyo@gmail.com>
2023-05-09 11:17:04 +0100

    Arithmetic operator

---
+++ c-self_revision/1-arithemtic_operator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* Arithmetic operators */
+
+int main()
+{
+	int num1 = 50;
+	int num2 = 200;
+	int sum;
+
+	//sum is num1 + num2
+	sum = num1 + num2;
+
+	printf("sum is : %d\n", sum);
+	return(0);
+}

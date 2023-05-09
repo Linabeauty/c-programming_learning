1c47906e0b4f7b8fc6dc0e408650cf523f87cae0
linabeauty <cabamiyo@gmail.com>
2023-05-09 11:24:53 +0100

    Subtration operator

---
+++ c-self_revision/2-substr_operator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Substration Arithemtic Operators
+
+int main()
+{
+	int x = 40;
+	int y = 300;
+	int sum;
+
+	//sum is x * y
+	sum = x * y;
+
+	printf("sum is %d\n", sum);
+	return(0);
+}

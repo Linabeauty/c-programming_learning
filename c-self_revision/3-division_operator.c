35d3e91bcf2bb8b949cc6dfa0600676d35e7cde5
linabeauty <cabamiyo@gmail.com>
2023-05-09 11:31:54 +0100

    Division operator

---
+++ c-self_revision/3-division_operator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//Division Arithmetic Operator
+
+int main()
+{
+	float x = 100.5;
+	float y = 20.5;
+	float sum;
+
+	//sum is x / y
+	sum = x / y;
+
+	printf("sum is %f\n", sum);
+	return(0);
+}

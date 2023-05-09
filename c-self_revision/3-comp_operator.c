e30a9ebf633ab365c0ea07243e1ea22dc8799097
linabeauty <cabamiyo@gmail.com>
2023-05-09 11:48:47 +0100

    Comparison operator

---
+++ c-self_revision/3-comp_operator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Comparison Arithmetic Operator
+
+int main()
+{
+	int x = 50;
+	int y = 150;
+
+	if ( x == y)
+		return(1);
+	else
+		printf("y is Greater!\n");
+	return(0);
+}

a17b24dd1bb153daa6bba74e74948e8284dbcb87
linabeauty <cabamiyo@gmail.com>
2023-05-09 13:13:25 +0100

    Logic operator

---
+++ c-self_revision/logic_operator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Logic operator , to determine which case is true or false
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int x = 50, y = 25;
+
+	if (x < 50 &&  x < 25)
+		printf("True!\n");
+	else
+		printf("Oh No Try Again!\n");
+	return (0);
+}

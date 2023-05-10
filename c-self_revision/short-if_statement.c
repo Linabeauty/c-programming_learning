e3783d79ef24048321dc3b03c2ce6afe1db24c94
linabeauty <cabamiyo@gmail.com>
2023-05-10 10:36:14 +0100

    Short if-statement

---
+++ c-self_revision/short-if_statement.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - used to write a short line of code instead
+ * of the long form of writing code
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int time = 15;
+
+	/* used to confirm if it's noon or evening */
+	(time > 15) ? printf("Good day.\n") : printf("Good evening.\n");
+	return (0);
+}

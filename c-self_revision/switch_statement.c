b0d164e953dce90fe7045b9637b622080ff174d8
linabeauty <cabamiyo@gmail.com>
2023-05-10 14:58:44 +0100

    Loops

---
+++ c-self_revision/switch_statement.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+/**
+ * main - swicth statement helps to reduce the normal
+ * way of writing if statement code in a long form
+ * @case: Uses case to choose which of the code 
+ * switch is matched to the requirement
+ * @break: Uses it switch between any case or code
+ * block that match and execute
+ * Return: 0
+ */
+
+int main()
+{
+	int emmaAge = 15;
+
+
+	switch (emmaAge)
+	case 1
+		printf("He is my first child\n");
+		break;
+
+	case 2
+		printf("He is my second child\n");
+		break;
+
+	case 3
+		printf("He is my third child\n");
+		break;
+
+}

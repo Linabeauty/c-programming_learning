4cc9bd54ffb69e08c90af4043d290178af963e6b
linabeauty <cabamiyo@gmail.com>
2023-05-11 18:47:32 +0100

    skip a number in loop

--- c-self_revision/skip-a_num.c
+++ c-self_revision/skip-a_num.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
 /**
- *
- *
+ * main - skip a certain number while executing the condition
+ * Return: 0
  */
+
 int main(void)
 {
+	int x = 1;
+
+	while (x <= 20)
+	{
+		if (x == 10)
+		{
+			x++;
+			continue;
+		}
+		printf("%d\n", x);
+		x++;
+
+	}
+	return (0);
+}

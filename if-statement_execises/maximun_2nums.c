71ed82a51d16c0c2329ef58ae277f78ba1fb6fae
linabeauty <cabamiyo@gmail.com>
2023-05-12 07:53:19 +0100

    Maximum among two numbers

---
+++ if-statement_execises/maximun_2nums.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - find the maximum among two numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x = 200;
+	int y = 350;
+
+	if (x > y)
+	{
+		printf("The maximum number is %d\n", x);
+	}
+
+	else
+	{
+		printf("The maximum number is %d\n", y);
+	}
+	return (0);
+}

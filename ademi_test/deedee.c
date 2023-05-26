eef3a26ecb7feb964c28517522ed36e1663056f6
linabeauty <cabamiyo@gmail.com>
2023-05-26 13:56:20 +0100

    update file

---
+++ ademi_test/deedee.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *  * main - testing new ubuntu installation without network connectivity
+ *   *
+ *    * Return: 0
+ *     */
+int main()
+{
+	int letter;
+
+	printf("Hello, World!");
+	return(0);
+}

3f00dc874226ca6c1506d9af834f75c92d8ba08f
linabeauty <cabamiyo@gmail.com>
2023-05-26 11:11:32 +0100

    Print naturals in different form

---
+++ loops-in_c_execises/2-natural_numbs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print natural numbers using for loop style
+ *
+ * Return: Always successful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}

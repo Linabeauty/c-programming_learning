c375e0c9131fba52c92a9326077be97e2a198b5b
linabeauty <cabamiyo@gmail.com>
2023-05-30 08:18:30 +0100

    Arrays

---
+++ arrays_in_c-programming/arrays_numb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print normal numbers using array to store data
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int myNumbers[] = { 20, 30, 40, 50, 60};
+	
+
+	printf("%d\n", myNumbers[2]);
+	return (0);
+}

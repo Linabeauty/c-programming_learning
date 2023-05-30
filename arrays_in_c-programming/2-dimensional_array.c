cc2d9a77b7247c23b3a65d570424703e17fe069d
linabeauty <cabamiyo@gmail.com>
2023-05-30 08:42:07 +0100

    2D dimensional Arrays

---
+++ arrays_in_c-programming/2-dimensional_array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print arrays in 2D dimensional 
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int matrix[2][3] = { {2, 3, 5}, {4, 5, 7} };
+	printf("%d\n", matrix[0][2]);
+}
---
+++ arrays_in_c-programming/2D-dimensiona_array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this 2D dimensional arrys print arrays and 
+ * also known as matrix
+ *
+ * Return: Always successful
+ */
+
+int main(void)
+{
+	int matrix[2][3] = { {3, 2, 5}, {4, 7,9} };
+	printf("%d\n", matrix[1][2]);
+}
